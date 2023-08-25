2c0f6da17e0c41fce098ba497c7bae6cc640e2c7
AzizDeveloper04 <saparbaev0131@gmail.com>
2023-08-25 17:32:23 +0500

    Learn: Change kescha

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ string greeting=$"hello {name}";
 System.Console.WriteLine(greeting);
 System.Console.WriteLine("Enter you age: ");
 int age=Convert.ToInt32(Console.ReadLine());
-
 System.Console.WriteLine("Enter the age of the kescha");
 int keschaage=Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"The difference in your ages is {Math.Abs(age-keschaage)}");
+System.Console.WriteLine($"Your age greater than kescha: {age>keschaage}");
